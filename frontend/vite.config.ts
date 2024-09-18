72a6281477cf0441c9fdc9658de875ae2be3984d
karim emad <karimkarim20444@gmail.com>
2024-09-18 15:57:47 -0700

    deploy back end

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     mimeTypes: {
-      'application/javascript': ['js']
+      'application/javascript': ['js', 'ts', 'tsx']
     }
   }
-})
+});
