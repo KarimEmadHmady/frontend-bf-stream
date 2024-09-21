29f26e8ef3989fa155bd2794fa80369270156c87
Web developer <94483684+KarimEmadHmady@users.noreply.github.com>
2024-09-20 19:38:15 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 const orderRoutes = require('./routes/orderRoutes'); // Import your order routes
 const Order = require('./models/Order'); // Adjust the path as necessary
 const userRoutes = require('./routes/userRoutes');
-const cron = require('node-cron');
+// const cron = require('node-cron');
 
 
 // Load environment variables
@@ -68,21 +68,21 @@ app.use('/api/orders', orderRoutes); // Register the order routes
 
 
 // Function to delete orders older than 20 hours
-const deleteOldOrders = async () => {
-  try {
-    const twentyHoursAgo = new Date(Date.now() - 20 * 60 * 60 * 1000); // 20 hours in milliseconds
-    await Order.deleteMany({ date: { $lt: twentyHoursAgo } });
-    console.log('Old orders deleted successfully');
-  } catch (error) {
-    console.error('Error deleting old orders:', error);
-  }
-};
+// const deleteOldOrders = async () => {
+//   try {
+//     const twentyHoursAgo = new Date(Date.now() - 20 * 60 * 60 * 1000); // 20 hours in milliseconds
+//     await Order.deleteMany({ date: { $lt: twentyHoursAgo } });
+//     console.log('Old orders deleted successfully');
+//   } catch (error) {
+//     console.error('Error deleting old orders:', error);
+//   }
+// };
 
 // Schedule the task to run every 20 hours
-cron.schedule('0 */20 * * *', () => {
-  console.log('Running scheduled task to delete old orders...');
-  deleteOldOrders();
-});
+// cron.schedule('0 */20 * * *', () => {
+//   console.log('Running scheduled task to delete old orders...');
+//   deleteOldOrders();
+// });
 
 
 
